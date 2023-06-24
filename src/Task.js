1532d4c8cde2829378917b9af1554f793c173bf5
detryjulindah <detryjulindah07@gmail.com>
2023-06-25 00:45:42 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Detry Julindah" // TODO: replace this
+const studentId = "FE4543997" // TODO: replace this
+
+export { url, studentName, studentId }
